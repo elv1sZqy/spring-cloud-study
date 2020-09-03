298ca760ccc70ca5672645efb35138118202b2e6
Elv1sQ1 <qiyun.zhu@itrigger.com>
2020-09-03 22:12:54 +0800

    测试config

--- feign-product/src/main/java/cn/study/feignproduct/controller/TestController.java
+++ feign-product/src/main/java/cn/study/feignproduct/controller/TestController.java
@@ -1,6 +1,8 @@
 package cn.study.feignproduct.controller;
 
 import cn.study.feignproduct.feign.TestFeign;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController implements TestFeign {
+    @Value("${test.name}")
+    private String name;
 
     @Override
     public String test(String s) {
         return s;
+    }
+
+    @GetMapping
+    public String getName(){
+        return name;
     }
 }
